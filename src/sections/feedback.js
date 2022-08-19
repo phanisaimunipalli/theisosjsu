b354f73e3b8008b64b6f00225ec972163f85a3d3
Phani Sai Ram Munipalli <phanisaimunipalli@gmail.com>
2022-08-18 17:15:35 -0700

    Updated feedback section

--- src/sections/feedback.js
+++ src/sections/feedback.js
@@ -81,9 +81,13 @@ export default function FeedbackCard() {
   return (
     <section id="feedback" sx={{ variant: 'section.testimonial'  }}>
       <Container>
-        <SectionHeader slogan="Feedback" title="Your Voice Matters" />
+        <SectionHeader slogan="Let us know what you think" title="Quick Feedback" />
       </Container>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdgaEFlYV9NnKmgfOmJQakFKJtKILjikFm-oWmMHqGBF3NTpA/viewform?embedded=true" width="1500" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <Box sx={styles.carouselWrapper}>
+            {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdgaEFlYV9NnKmgfOmJQakFKJtKILjikFm-oWmMHqGBF3NTpA/viewform?embedded=true" width="1500" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+      <iframe frameborder="0" width="800" height="800" textAlign="center" src="https://s.surveyplanet.com/62fed271ce16007705a794aa" frameborder="0"></iframe>
+      </Box>
+
     </section>
   );
 }
@@ -101,122 +105,8 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-end',
     flexDirection: 'column',
-    alignItems: 'flex-end',
+    alignItems: 'center',
     mt: '-35px',
-    px: '15px',
-    '.carousel-container': {
-      width: '100%',
-      maxWidth: [
-        '100%',
-        null,
-        null,
-        '750px',
-        '1000px',
-        '1180px',
-        null,
-        'calc(50% + 865px)',
-      ],
-      mr: ['auto', null, null, null, null, null, null, '-220px'],
-      ml: 'auto',
-      '.react-multi-carousel-item': {
-        transition: 'all 0.25s',
-      },
-      '.react-multi-carousel-item--active:nth-of-type(4n)': {
-        opacity: '0.5',
-        '@media screen and (max-width: 1620px)': {
-          opacity: 1,
-        },
-      },
-    },
-  },
-  reviewCard: {
-    boxShadow: '0px 0px 1px rgba(38, 78, 118, 0.35)',
-    transition: 'all 0.3s',
-    borderRadius: '6px',
-    p: [
-      '30px 20px 35px',
-      '30px 25px 35px',
-      '30px 20px 35px',
-      '35px 30px 40px 40px',
-      '30px 30px 35px',
-      '35px 30px 40px 40px',
-    ],
-    bg: 'white',
-    textAlign: 'left',
-    m: [
-      '28px 5px 30px 5px',
-      '28px 20px 30px 20px',
-      '28px 15px 30px 15px',
-      '28px 15px 30px 15px',
-      '30px 20px 40px',
-    ],
-    '&:hover': {
-      boxShadow: '0px 6px 47px rgba(38, 78, 118, 0.1)',
-    },
-    '.rating': {
-      mb: [1, null, null, 2],
-      ul: {
-        pl: 0,
-        listStyle: 'none',
-        mb: 0,
-        display: 'flex',
-      },
-      svg: {
-        marginRight: '2px',
-        '&:last-of-type': {
-          marginRight: 0,
-        },
-      },
-      '.star': {
-        color: 'yellow',
-        mr: '1px',
-      },
-      '.star-o': {
-        color: '#ddd',
-        mr: '1px',
-      },
-    },
-    '.card-footer': {
-      display: 'flex',
-      alignItems: 'center',
-      marginTop: [5, null, null, '33px'],
-      '.image': {
-        flexShrink: 0,
-        mr: [3, null, null, 4],
-        display: 'flex',
-        img: {
-          width: '55px',
-          height: '55px',
-          borderRadius: '50%',
-          objectFit: 'cover',
-        },
-      },
-    },
-  },
-  title: {
-    fontSize: [1, 5],
-    fontWeight: 700,
-    mb: [3, null, null, '22px'],
-    color: 'text',
-    lineHeight: 1.6,
-  },
-  description: {
-    fontSize: [1, null, null, 2],
-    fontWeight: 'normal',
-    color: 'text',
-    lineHeight: [1.85, null, 2],
-  },
-  heading: {
-    fontSize: [1, null, null, 2],
-    fontWeight: 700,
-    mb: '3px',
-    color: 'text',
-    lineHeight: 1.3,
-  },
-  designation: {
-    color: '#25A0FF',
-    fontWeight: '500',
-    fontSize: 1,
-    lineHeight: 1.4,
-  },
-};
+    px: '15px'
+}
+}
